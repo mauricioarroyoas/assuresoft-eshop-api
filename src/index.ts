1104eb80f4d2760c556fe627b30dc5865aac060b
mauricioarrequena <mauricio.ar.requena@gmail.com>
2025-04-21 00:41:49 -0400

    feat:added typeorm initlization at the start of the index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { app } from "./app";
+import { AppDataSource } from "./data-source";
 
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("error during data source initilization", error);
+  });
