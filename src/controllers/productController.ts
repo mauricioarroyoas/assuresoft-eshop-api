db09301a8f4f9567dec34d83fa3c2db13f615358
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-04-21 16:44:13 -0400

    Feat: add updateField to ProductService and patch request to controller.

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -69,3 +69,15 @@ productController.get("/:id", async (req, res) => {
     res.status(404).json({error: "product doesn't exist"})
   }
 })
+
+productController.patch("/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const fieldUpdated = await productService.updateField(id, req.body);
+    res.status(200).json(fieldUpdated);
+  } catch (error) {
+    res.status(400).json({
+      error: error instanceof Error ? error.message : "Unprocesable Entity"
+    })
+  }
+})
