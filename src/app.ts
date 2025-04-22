5f780f050ff75d1ff110cffd16d86fcaddcd3b55
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-04-22 15:28:18 -0400

    feature: add swagger config.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './docs/swagger';
 import { productController } from "./controllers/productController";
 
+// App
 export const app = express();
 app.use(express.json());  // Middleware to parse JSON
+
+// Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Routes
 app.use('/products', productController);
