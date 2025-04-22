3a2a4f5f07d8275116c40ae5c10cdaac2945187a
Mauricio Arroyo <mauricioarroyo@ascbomacmini76.local>
2025-04-22 11:58:52 -0400

    config: changed config of test so it shows test coverage only when asked

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "jest";
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverage: true,
   collectCoverageFrom: [
     "src/**/*.ts",             // adjust this to match your code folder
     "!src/**/index.ts",        // optional: ignore entry points
